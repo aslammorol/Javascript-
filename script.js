537fda48ad46da27dee939949a3ec84a3c72b8f3
Aslam Morol <aslammoroldvmpstu@gmail.com>
2024-02-01 10:41:26 +0600

    most frequent elements in array

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-
+const myArray = [1, 3,2,2,4,2,6,3,2,2,2];
+ myArray.sort((a, b) => a - b);
+ console.log(myArray);
+ let count = 1;
+ let max = 0;
+ let elemnt;
+ for (let i = 1; i < myArray.length; i++) {
+   if (myArray[i] === myArray[i - 1]) {
+     count++;
+   }else{
+     count = 1;
+   }
+   if (count > max ) {
+     max = count;
+     elemnt = myArray[i];
+   }
+ }
+ console.log(`item : ${elemnt}, occurance : ${max}`); 
+ 
