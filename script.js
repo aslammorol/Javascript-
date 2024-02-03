8bc86240f209b778d8586686896fbdb76841bd88
aslammorol <aslammoroldvmpstu@gmail.com>
2024-02-03 16:38:46 +0600

    hdjd

--- script.js
+++
@@ -1,9 +0,0 @@
-const numbers = [45, 4, 9, 16, 25];
-let txt = 0;
-for (let x in numbers) {
-    txt += numbers[x];
-}
-// for(let i = 0; i < numbers.length; i++){
-//   txt += numbers[i];
-// }
-console.log(txt);
