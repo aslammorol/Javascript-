102e059a2bee9a86e6d3513209d12422b218f5e1
aslammorol <aslammoroldvmpstu@gmail.com>
2024-02-09 16:56:15 +0600

    promise

---
+++ asynchronous/promise.js
@@ -0,0 +1,50 @@
+ const payment = true;
+ const marks = 80;
+ 
+ function enroll() {
+   console.log("Enrollment is in progress");
+   const promise = new Promise(function(resolve,reject) {
+     setTimeout(function() {
+       if (payment) {
+         resolve();
+       }else{
+         reject('Payment failed');
+       }
+     }, 2000);
+   });
+   return promise;
+ }
+ 
+ function progress(){
+   console.log("Course is in progress...");
+   const promise = new Promise(function(resolve,reject){
+     setTimeout(function() {
+       if(marks >= 80){
+         resolve();
+       }else{
+         reject('Sorry! You couldn’t get enough marks to get certificate');
+       }
+     }, 3000);
+   });
+   return promise;
+ }
+ 
+ function getCertificate() {
+   console.log('Getting certificate ready for you...');
+   const promise = new Promise(function(resolve){
+     setTimeout(function() {
+       resolve("Congrats! you got the certificate");
+     }, 2000);
+   });
+   return promise;
+ }
+ 
+ enroll()
+        .then(progress)
+        .then(getCertificate)
+        .then(function(val){
+          console.log(val);
+        })
+        .catch(function(val){
+          console.log(val);
+        });
